7eab9d99efee767899fed9813b86d693d5c03f51
天南 <tiannan@wacai.com>
2018-11-06 12:20:37 +0800

    env config completed

--- app/renderer.jsx
+++ app/renderer.jsx
@@ -2,11 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css"
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 
 ReactDOM.render((
   <div>
     {/* asdfasdf */}
     <Button>Click Here</Button>
+    <Button animated='vertical'>
+      <Button.Content hidden>Shop</Button.Content>
+      <Button.Content visible>
+        <Icon name='shop' />
+      </Button.Content>
+    </Button>
   </div>
 ), document.querySelector("app"));
